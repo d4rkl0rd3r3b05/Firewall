8f7dd2bae12a2547a48f0734ed51c501a2b22851
Mayank <gupta.mayank@99acres.com>
2014-10-28 00:50:19 +0530

    Code corrected

--- Firewall/credit.py
+++ Firewall/credit.py
@@ -21,26 +21,8 @@ def crd(prntwin):
 	mtxt="MAYANK GUPTA"+"\n"+"  (AKA D4RKL0RD)"+"\t"*2
 	
 	Llbl1=Label(Bftfrm,text=mtxt,relief=SOLID,borderwidth=0,bg="white",fg="black",font=fnt)
-	Llbl1.pack(side=LEFT,expand=YES,fill=X,anchor=W)
-	Bftfrm.pack(side=LEFT,anchor=W)
-
-
-	#Bstfrm
-	Bstfrm=Frame(Btfrm,relief=SOLID,borderwidth=0,bg="white")
-	vtxt="\n"+"\t"+"VIKAS KUMAR"+"\n"+"\t"*2
-	Llbl2=Label(Bstfrm,text=vtxt,relief=SOLID,borderwidth=0,bg="white",fg="black",font=fnt)
-	Llbl2.pack(side=TOP,expand=YES,fill=X,anchor=CENTER)
-	Bstfrm.pack(side=LEFT,anchor=CENTER)
-
-
-	#Bttfrm
-	Bttfrm=Frame(Btfrm,relief=SOLID,borderwidth=0,bg="white")
-	rtxt="\n"+"\t"+"RAHUL SONKAR"+"\n"+"\t"*2
-	
-	Llbl3=Label(Bttfrm,text=rtxt,relief=SOLID,borderwidth=0,bg="white",fg="black",font=fnt)
-	Llbl3.pack(side=TOP,expand=YES,fill=X,anchor=W)
-	Bttfrm.pack(side=RIGHT,anchor=E)
-
+	Llbl1.pack(side=TOP,expand=YES,fill=X,anchor=CENTER)
+	Bftfrm.pack(side=LEFT,anchor=CENTER)
 
 	Btfrm.pack(side=LEFT,anchor=W)	
 	rootl.pack(side=LEFT,anchor=W,expand=YES,fill=Y) 
